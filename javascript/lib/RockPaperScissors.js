05c27fa6f306a4c8a3d8ebcf3740225b5d5f7ec1
Michiel Stigter <michielstigter@hotmail.com>
2014-08-04 12:25:18 +0100

    made it working

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -12,6 +12,21 @@ function Game(player1, player2) {
   this.player2 = player2;
 };
 
+;
+
+PAIRS = {"rock":"scissors", "scissors":"rock", "paper":"rock", "scissors":"paper"}
+
 Game.prototype.winner = function() {
-  return this.player1;
+  if (this.same_pick()) {
+  	return null;
+  } else if (PAIRS[this.player1.pick] == this.player2.pick) {
+	  return this.player1;
+	} else {
+		return this.player2;
+	}
+}
+
+
+Game.prototype.same_pick = function() {
+	return(this.player1.pick == this.player2.pick);
 };
